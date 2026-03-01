a5b0d0a0eb03da2dfb7180b918b3a76d29c7e996
Colin Skow <skow0020@gmail.com>
2026-03-01 11:02:24 -0600

    Update linting and add workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextPlugin from "@next/eslint-plugin-next";
+import tseslint from "typescript-eslint";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...tseslint.configs.recommended,
+  nextPlugin.configs["core-web-vitals"],
+  {
+    rules: {
+      "no-undef": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
-    // Default ignores of eslint-config-next:
+    // Default ignores aligned with Next.js projects:
     ".next/**",
     "out/**",
     "build/**",
